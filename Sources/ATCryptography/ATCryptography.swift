897bc89079f9bb6f4fc53b97ade4e23ec685cf41
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-03-04 13:07:34 -0500

    Add main structures

--- Sources/ATCryptography/ATCryptography.swift
+++ Sources/ATCryptography/ATCryptography.swift
@@ -1,2 +1,22 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  Cryptography.swift
+//  ATButler
+//
+//  Created by Christopher Jr Riley on 2025-03-03.
+//
+
+import Foundation
+import CryptoKit
+import Crypto
+
+///
+class ATCryptography {
+    static let base58MultibasePrefix: Character = "z"
+    static let didKeyPrefix: String = "did:key:"
+
+    static let secp256k1DIDPrefix: [UInt8] = [0xe7, 0x01]
+    static let p256DIDPrefix: [UInt8] = [0x80, 0x24]
+
+    static let p256JWTAlgorithm: String = "ES256"
+    static let secp256k1JWTAlgorithm: String = "ES256K"
+}
