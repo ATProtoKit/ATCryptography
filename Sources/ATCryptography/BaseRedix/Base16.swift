939e624c5c4c050b63d06d957de10168d3137f56
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-03-06 00:57:37 -0500

    Refactor Base16 to accept UInt8

--- Sources/ATCryptography/BaseRedix/Base16.swift
+++ Sources/ATCryptography/BaseRedix/Base16.swift
@@ -10,44 +10,44 @@ import Foundation
 /// A utility for encoding and decoding Base16 (hexadecimal) data.
 public struct Base16 {
 
-    /// Converts bnary data to a Base16 string representation.
+    /// Converts an array of bytes to a Base16 string representation.
     ///
-    /// - Parameter data: The input data to encode.
+    /// - Parameter bytes: The input byte array to encode.
     /// - Returns: The encoded `String` in Base16.
-    public static func encode(_ data: Data) -> String {
-        let input = data.map { String(format: "%02x", $0) }.joined()
+    public static func encode(_ bytes: [UInt8]) -> String {
+        return bytes.map { String(format: "%02x", $0) }.joined()
     }
 
-    /// Converts binary data to a Base16 string representation (uppercase).
+    /// Converts an array of bytes to a Base16 string representation (uppercase).
     ///
-    /// - Parameter data: The input data to encode.
+    /// - Parameter bytes: The input byte array to encode.
     /// - Returns: The encoded `String` in uppercase Base16.
-    public static func encodeUpper(_ data: Data) -> String {
-        return data.map { String(format: "%02X", $0) }.joined()
+    public static func encodeUpper(_ bytes: [UInt8]) -> String {
+        return bytes.map { String(format: "%02X", $0) }.joined()
     }
 
     /// Converts a Base16 string to binary data.
     ///
     /// - Parameter input: The Base16 string to decode.
-    /// - Returns: The raw bytes as a `Data` object, or `nil` if the string is invalid.
+    /// - Returns: The raw bytes as an array of `UInt8`, or `nil` if the string is invalid.
     ///
     /// - Note: The input string must contain an even number of characters
     ///   and only consist of valid hexadecimal digits (0-9, A-F, a-f). If
     ///   these conditions are not met, the function returns `nil`.
-    public static func decode(_ string: String) -> Data? {
+    public static func decode(_ string: String) -> [UInt8]? {
         let length = string.count
         guard length % 2 == 0 else { return nil }
 
-        var data = Data(capacity: length / 2)
+        var bytes = [UInt8]()
         var index = string.startIndex
 
-        for _ in 0..<length / 2 {
+        for _ in 0..<(length / 2) {
             let nextIndex = string.index(index, offsetBy: 2)
             guard let byte = UInt8(string[index..<nextIndex], radix: 16) else { return nil }
-            data.append(byte)
+            bytes.append(byte)
             index = nextIndex
         }
 
-        return data
+        return bytes
     }
 }
