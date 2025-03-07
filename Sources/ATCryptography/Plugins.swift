36cfe6e4294bb4ab4b13c8f7fa49402f51a0ed55
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-03-07 00:41:28 -0500

    Add plugins typealias

---
+++ Sources/ATCryptography/Plugins.swift
@@ -0,0 +1,15 @@
+//
+//  Plugins.swift
+//  ATCryptography
+//
+//  Created by Christopher Jr Riley on 2025-03-07.
+//
+
+import Foundation
+
+/// A collection of supported `did:key` plugins.
+public typealias plugins = [
+    P256Plugin
+    // TODO: Add this in once we have the struct implemented.
+    // P256Plugin
+]
