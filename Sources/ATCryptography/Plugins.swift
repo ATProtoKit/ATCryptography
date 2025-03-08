32dafdb3f338cd382f350b055d3597e87eac7c93
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-03-08 02:25:47 -0500

    Add K256Plugin to "plugins" variable

--- Sources/ATCryptography/Plugins.swift
+++ Sources/ATCryptography/Plugins.swift
@@ -9,7 +9,6 @@ import Foundation
 
 /// A collection of supported `did:key` plugins.
 public let plugins: [any DIDKeyPlugin.Type] = [
-    P256Plugin.self
-    // TODO: Uncomment this when the struct has been implemented.
-//    K256Plugin.self
+    P256Plugin.self,
+    K256Plugin.self
 ]
