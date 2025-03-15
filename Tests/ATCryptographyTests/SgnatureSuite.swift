c4571e632edcbe4a2b01dc1dc8df979c3c9581e6
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-03-15 03:05:18 -0400

    Move test vectors to main struct

--- Tests/ATCryptographyTests/SgnatureSuite.swift
+++ Tests/ATCryptographyTests/SgnatureSuite.swift
@@ -11,10 +11,8 @@ import Testing
 
 @Suite("Signatures", .disabled()) struct SignatureTests {
 
-    @MainActor public static var signatureVectors: [TestVector] = []
-
-    @Test("Verifies k256 and p256 signature vectors.", arguments: signatureVectors, 1...signatureVectors.count)
-    func verifySignatureVectors(signatureVector: TestVector) async throws {
+    @Test("Verifies k256 and p256 signature vectors.", arguments: TestVectorEnum.signatureVectors, 1...TestVectorEnum.signatureVectors.count)
+    func verifySignatureVectors(signatureVector: TestVector, count: Int) async throws {
         let messageBytes = signatureVector.base64Message.data(using: .utf8)?.base64EncodedString()
         let signatureBytes = signatureVector.base64Signature.data(using: .utf8)?.base64EncodedString()
         let keyBytes = try Multibase.multibaseToBytes(multibase: signatureVector.publicMultibaseKey)
@@ -45,6 +43,67 @@ import Testing
                 }
             default:
                 break
+        }
+    }
+
+    enum TestVectorEnum {
+        public static var signatureVectors: [TestVector] {
+            return [
+                TestVector(
+                    algorithm: "ES256",
+                    publicDIDKey: "did:key:zDnaembgSGUhZULN2Caob4HLJPaxBh92N7rtH21TErzqf8HQo",
+                    publicMultibaseKey: "zxdM8dSstjrpZaRUwBmDvjGXweKuEMVN95A9oJBFjkWMh",
+                    base64Message: "oWVoZWxsb2V3b3JsZA",
+                    base64Signature: "2vZNsG3UKvvO/CDlrdvyZRISOFylinBh0Jupc6KcWoJWExHptCfduPleDbG3rko3YZnn9Lw0IjpixVmexJDegg",
+                    isSignatureValid: true,
+                    tags: []
+                ),
+                TestVector(
+                    algorithm: "ES256K",
+                    publicDIDKey: "did:key:zQ3shqwJEJyMBsBXCWyCBpUBMqxcon9oHB7mCvx4sSpMdLJwc",
+                    publicMultibaseKey: "z25z9DTpsiYYJKGsWmSPJK2NFN8PcJtZig12K59UgW7q5t",
+                    base64Message: "oWVoZWxsb2V3b3JsZA",
+                    base64Signature: "5WpdIuEUUfVUYaozsi8G0B3cWO09cgZbIIwg1t2YKdUn/FEznOndsz/qgiYb89zwxYCbB71f7yQK5Lr7NasfoA",
+                    isSignatureValid: true,
+                    tags: []
+                ),
+                TestVector(
+                    algorithm: "ES256",
+                    publicDIDKey: "did:key:zDnaembgSGUhZULN2Caob4HLJPaxBh92N7rtH21TErzqf8HQo",
+                    publicMultibaseKey: "zxdM8dSstjrpZaRUwBmDvjGXweKuEMVN95A9oJBFjkWMh",
+                    base64Message: "oWVoZWxsb2V3b3JsZA",
+                    base64Signature: "2vZNsG3UKvvO/CDlrdvyZRISOFylinBh0Jupc6KcWoKp7O4VS9giSAah8k5IUbXIW00SuOrjfEqQ9HEkN9JGzw",
+                    isSignatureValid: false,
+                    tags: ["high-s"]
+                ),
+                TestVector(
+                    algorithm: "ES256K",
+                    publicDIDKey: "did:key:zQ3shqwJEJyMBsBXCWyCBpUBMqxcon9oHB7mCvx4sSpMdLJwc",
+                    publicMultibaseKey: "z25z9DTpsiYYJKGsWmSPJK2NFN8PcJtZig12K59UgW7q5t",
+                    base64Message: "oWVoZWxsb2V3b3JsZA",
+                    base64Signature: "5WpdIuEUUfVUYaozsi8G0B3cWO09cgZbIIwg1t2YKdXYA67MYxYiTMAVfdnkDCMN9S5B3vHosRe07aORmoshoQ",
+                    isSignatureValid: false,
+                    tags: ["high-s"]
+                ),
+                TestVector(
+                    algorithm: "ES256",
+                    publicDIDKey: "did:key:zDnaeT6hL2RnTdUhAPLij1QBkhYZnmuKyM7puQLW1tkF4Zkt8",
+                    publicMultibaseKey: "ze8N2PPxnu19hmBQ58t5P3E9Yj6CqakJmTVCaKvf9Byq2",
+                    base64Message: "oWVoZWxsb2V3b3JsZA",
+                    base64Signature: "MEQCIFxYelWJ9lNcAVt+jK0y/T+DC/X4ohFZ+m8f9SEItkY1AiACX7eXz5sgtaRrz/SdPR8kprnbHMQVde0T2R8yOTBweA",
+                    isSignatureValid: false,
+                    tags: ["der-encoded"]
+                ),
+                TestVector(
+                    algorithm: "ES256K",
+                    publicDIDKey: "did:key:zQ3shnriYMXc8wvkbJqfNWh5GXn2bVAeqTC92YuNbek4npqGF",
+                    publicMultibaseKey: "z22uZXWP8fdHXi4jyx8cCDiBf9qQTsAe6VcycoMQPfcMQX",
+                    base64Message: "oWVoZWxsb2V3b3JsZA",
+                    base64Signature: "MEUCIQCWumUqJqOCqInXF7AzhIRg2MhwRz2rWZcOEsOjPmNItgIgXJH7RnqfYY6M0eg33wU0sFYDlprwdOcpRn78Sz5ePgk",
+                    isSignatureValid: false,
+                    tags: ["der-encoded"]
+                )
+            ]
         }
     }
 
