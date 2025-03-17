be4bdde6d74dcfdcf7b1fb22f563b35f584b38d6
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-03-17 14:44:26 -0400

    Change test vectors to an enum
    This should hopefully get rid of the data race errors.

--- Tests/ATCryptographyTests/DIDKeySuite.swift
+++ Tests/ATCryptographyTests/DIDKeySuite.swift
@@ -84,39 +84,47 @@ import Testing
                     "The array of bytes in the parsed did:key should match the array of bytes in the keypair.")
         }
     }
-}
 
-struct EllipticalCurveTestVectors {
+    enum EllipticalCurveTestVectors {
 
-    // did:key k256 test vectors from W3C.
-    // https://github.com/w3c-ccg/did-method-key/blob/main/test-vectors/secp256k1.json
-    static let k256Seeds: [String] = [
-        "9085d2bef69286a6cbb51623c8fa258629945cd55ca705cc4e66700396894e0c",
-        "f0f4df55a2b3ff13051ea814a8f24ad00f2e469af73c363ac7e9fb999a9072ed",
-        "6b0b91287ae3348f8c2f2552d766f30e3604867e34adc37ccbb74a8e6b893e02",
-        "c0a6a7c560d37d7ba81ecee9543721ff48fea3e0fb827d42c1868226540fac15",
-        "175a232d440be1e0788f25488a73d9416c04b6f924bea6354bf05dd2f1a75133"
-    ]
+        // did:key k256 test vectors from W3C.
+        // https://github.com/w3c-ccg/did-method-key/blob/main/test-vectors/secp256k1.json
+        static var k256Seeds: [String] {
+            return [
+                "9085d2bef69286a6cbb51623c8fa258629945cd55ca705cc4e66700396894e0c",
+                "f0f4df55a2b3ff13051ea814a8f24ad00f2e469af73c363ac7e9fb999a9072ed",
+                "6b0b91287ae3348f8c2f2552d766f30e3604867e34adc37ccbb74a8e6b893e02",
+                "c0a6a7c560d37d7ba81ecee9543721ff48fea3e0fb827d42c1868226540fac15",
+                "175a232d440be1e0788f25488a73d9416c04b6f924bea6354bf05dd2f1a75133"
+            ]
+        }
 
-    // did:key k256 test vectors from W3C.
-    // https://github.com/w3c-ccg/did-method-key/blob/main/test-vectors/secp256k1.json
-    static let k256IDs: [String] = [
-        "did:key:zQ3shokFTS3brHcDQrn82RUDfCZESWL1ZdCEJwekUDPQiYBme",
-        "did:key:zQ3shtxV1FrJfhqE1dvxYRcCknWNjHc3c5X1y3ZSoPDi2aur2",
-        "did:key:zQ3shZc2QzApp2oymGvQbzP8eKheVshBHbU4ZYjeXqwSKEn6N",
-        "did:key:zQ3shadCps5JLAHcZiuX5YUtWHHL8ysBJqFLWvjZDKAWUBGzy",
-        "did:key:zQ3shptjE6JwdkeKN4fcpnYQY3m9Cet3NiHdAfpvSUZBFoKBj"
-    ]
+        // did:key k256 test vectors from W3C.
+        // https://github.com/w3c-ccg/did-method-key/blob/main/test-vectors/secp256k1.json
+        static var k256IDs: [String] {
+            return [
+                "did:key:zQ3shokFTS3brHcDQrn82RUDfCZESWL1ZdCEJwekUDPQiYBme",
+                "did:key:zQ3shtxV1FrJfhqE1dvxYRcCknWNjHc3c5X1y3ZSoPDi2aur2",
+                "did:key:zQ3shZc2QzApp2oymGvQbzP8eKheVshBHbU4ZYjeXqwSKEn6N",
+                "did:key:zQ3shadCps5JLAHcZiuX5YUtWHHL8ysBJqFLWvjZDKAWUBGzy",
+                "did:key:zQ3shptjE6JwdkeKN4fcpnYQY3m9Cet3NiHdAfpvSUZBFoKBj"
+            ]
+        }
 
-    // did:key p256 test vectors from W3C.
-    // https://github.com/w3c-ccg/did-method-key/blob/main/test-vectors/nist-curves.json
-    static let p256PrivateKeys: [String] = [
-        "9p4VRzdmhsnq869vQjVCTrRry7u4TtfRxhvBFJTGU2Cp"
-    ]
+        // did:key p256 test vectors from W3C.
+        // https://github.com/w3c-ccg/did-method-key/blob/main/test-vectors/nist-curves.json
+        static var p256PrivateKeys: [String] {
+            return [
+                "9p4VRzdmhsnq869vQjVCTrRry7u4TtfRxhvBFJTGU2Cp"
+            ]
+        }
 
-    // did:key p256 test vectors from W3C.
-    // https://github.com/w3c-ccg/did-method-key/blob/main/test-vectors/nist-curves.json
-    static let p256TestVectorsIDs: [String] = [
-        "did:key:zDnaeTiq1PdzvZXUaMdezchcMJQpBdH2VN4pgrrEhMCCbmwSb"
-    ]
+        // did:key p256 test vectors from W3C.
+        // https://github.com/w3c-ccg/did-method-key/blob/main/test-vectors/nist-curves.json
+        static var p256TestVectorsIDs: [String] {
+            return [
+                "did:key:zDnaeTiq1PdzvZXUaMdezchcMJQpBdH2VN4pgrrEhMCCbmwSb"
+            ]
+        }
+    }
 }
